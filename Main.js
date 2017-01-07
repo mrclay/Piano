bccb3f9f19ee753943e3ecc585c11ca2f10a6727
Steve Clay <steve@mrclay.org>
2017-01-07 10:09:26 -0500

    Cleanup, reduce needed samples

--- Main.js
+++ Main.js
@@ -1,43 +1,56 @@
 import Piano from 'Piano'
-import Master from 'Tone/core/Master'
+//import Master from 'Tone/core/Master'
 import Buffer from 'Tone/core/Buffer'
 
-var piano = new Piano([21, 108], 5).toMaster();
+var RANGE = [36, 96];
+var VELOCITIES = 1;
+var USE_RELEASE = false;
+
+var piano = new Piano(RANGE, VELOCITIES, USE_RELEASE).toMaster();
+var playingNotes = [];
 
 piano.load('./Salamander/').then(() => {
-	//#n=41,48,55,60,51,58&c=F11
 	var m = location.hash.match(/n=([\d,]+)&c=(.*)/);
 	if (m) {
 		m[1].split(',').forEach((note) => {
-			document.querySelector('[data-note="' + note + '"]').classList.add('active');
+			toggleKey(note, false);
 		});
-		document.querySelector('#chord').value = m[2];
+		document.querySelector('#chord').value = decodeURIComponent(m[2]);
 	}
 });
 
-function getNotes() {
+function getDepressedKeys() {
 	var notes = [];
-
 	document.querySelectorAll('[data-note].active').forEach((el) => {
 		notes.push(parseInt(el.dataset.note, 10));
 	});
-
 	return notes;
 }
 
-function play() {
-
-	var notes = getNotes();
-
+function playAll() {
+	stopAll();
+	var notes = getDepressedKeys();
 	notes.forEach((note) => {
 		piano.keyDown(note);
+		playingNotes.push(note);
 	});
+}
 
-	setTimeout(() => {
-		notes.forEach((note) => {
-			piano.keyUp(note);
-		});
-	}, 2000);
+function stopAll() {
+	playingNotes.forEach((note) => {
+		piano.keyUp(note);
+	});
+	playingNotes = [];
+}
+
+function toggleKey(value, playAfter = true) {
+	var el = document.querySelector('[data-note="' + value + '"]');
+	if (el.classList.contains('active')) {
+		el.classList.remove('active');
+	} else {
+		el.classList.add('active');
+	}
+	playAfter && playAll();
 }
 
 /**
@@ -54,34 +67,47 @@ Buffer.on('load', (prog) => {
 /**
  *  MIDI INPUT
  */
-function parseInput(message){
-	if (message[0] === 176 && message[1] == 64){
-		if (message[2] > 0) {
-			piano.pedalDown();
-		} else {
-			piano.pedalUp();
-		}
-	} else if (message[0] === 128){ //noteOff
-		piano.keyUp(message[1]);
-	} else if (message[0] === 144){ //noteOn
-		piano.keyDown(message[1], message[2] / 127);
-	}
-}
-
 if (navigator.requestMIDIAccess) {
     navigator.requestMIDIAccess().then((midiAccess) => {
     	midiAccess.inputs.forEach((input) => {
     		input.addEventListener('midimessage', (e) => {
-    			parseInput(e.data)
+				if (e.data[0] === 144 && e.data[2] != 0) {
+					toggleKey(e.data[1]);
+				}
     		})
     	});
     });
 }
 
+window.addEventListener('click', (e) => {
+	if (e.target.dataset.note) {
+		toggleKey(e.target.dataset.note);
+		return false;
+	}
+
+	if (e.target.id == 'play') {
+		playAll();
+		return false;
+	}
+
+	if (e.target.id == 'stop') {
+		stopAll();
+		return false;
+	}
+
+	if (e.target.id == 'save') {
+		var chord = document.querySelector('#chord').value;
+		var notes = getDepressedKeys();
+
+		location.hash = '#n=' + notes.join(',') + '&c=' + encodeURIComponent(chord);
+		location.reload();
+	}
+});
+
 window.addEventListener('DOMContentLoaded', () => {
 	var whites = '', blacks = '', note, mod, left = 36;
 
-	for (note = 36; note < 97; note++) {
+	for (note = RANGE[0]; note <= RANGE[1]; note++) {
 		mod = note % 12;
 		if (mod == 1 || mod == 3 || mod == 6 || mod == 8 || mod == 10) {
 			blacks += '<div data-note=' + note + ' style="left:' + left + 'px"></div>';
@@ -96,29 +122,4 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	document.querySelector('.white').innerHTML = whites;
 	document.querySelector('.black').innerHTML = blacks;
-});
-
-window.addEventListener('click', (e) => {
-	if (e.target.dataset.note) {
-		if (e.target.classList.contains('active')) {
-			e.target.classList.remove('active');
-		} else {
-			e.target.classList.add('active');
-		}
-		play();
-		return false;
-	}
-
-	if (e.target.id == 'play') {
-		play();
-		return false;
-	}
-
-	if (e.target.id == 'save') {
-		var chord = document.querySelector('#chord').value;
-		var notes = getNotes();
-
-		location.hash = '#n=' + notes.join(',') + '&c=' + encodeURIComponent(chord);
-		location.reload();
-	}
 });
